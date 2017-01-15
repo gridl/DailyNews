1f3bbcd3a723be595e5dbbce2dbfb562a6361be3
Subramanian <vishwanath79@outlook.com>
2017-01-15 13:11:36 -0800

    daily news

--- newsrobot.py
+++ newsrobot.py
@@ -1,25 +1,53 @@
-import requests
 import json
 import logging
-from config import key
+import smtplib
+
 import colorlog
+import requests
+
+from config import key, send, rec, api
 
 logger = colorlog.getLogger()
 newssources = ['the-wall-street-journal', 'techcrunch', 'new-york-magazine', 'bloomberg', 'financial-times',
-               'reddit-r-all', 'the-times-of-india','business-insider']
+               'reddit-r-all', 'the-times-of-india', 'business-insider']
+
+posts = []
 
 
-def get_news():
+def get_news_test():
     for newssource in newssources:
-
-        r = requests.get(f"https://newsapi.org/v1/articles?source={newssource}&sortBy=top&apiKey={key}")
-
+        r = requests.get(f"https://newsapi.org/v1/articles?source={newssource}&sortBy=top&apiKey={api}")
         data = json.loads(r.text)
-
         logging.info(f"Getting {newssource}\n")
         print("\n-------------------" + (newssource.upper()) + "-------------------")
         for article in (data['articles']):
             print(article['title'], article['url'])
+
+
+def get_news():
+    for newssource in newssources:
+        r = requests.get(f"https://newsapi.org/v1/articles?source={newssource}&sortBy=top&apiKey={api}")
+        data = json.loads(r.text)
+        logging.info(f"Getting {newssource}\n")
+        posts.append(newssource.upper())
+        for article in (data['articles']):
+            output = str(article['title'] + " " + article['url'] + " " + str(article['publishedAt']))
+            posts.append(output)
+        posts.append('\n')
+    return '\n'.join(posts).encode('utf-8')
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(send, key)
+    #msg = 'Subject: %s\n\n%s' % (SUBJECT, get_posts())
+    logger.info("Getting news")
+    msg = get_news()
+    server.sendmail(send, rec, msg)
+    logger.info("Sending mail")
+    server.quit()
 
 
 if __name__ == '__main__':
@@ -27,4 +55,5 @@ if __name__ == '__main__':
     handler = colorlog.StreamHandler()
     handler.setFormatter(colorlog.ColoredFormatter())
     logger.addHandler(handler)
-    get_news()
+    send_mail()
+    # get_news_test()
